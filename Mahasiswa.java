a9693e58ec9afd21f9677e3d2b2d67e84109d706
AyangNovaAnggraeni <165558797+AyangNovaAnggraeni@users.noreply.github.com>
2024-08-30 17:32:46 +0700

    Mahasiswa.java

---
+++ Mahasiswa.java
@@ -0,0 +1,20 @@
+public class Mahasiswa {
+    int NPM;
+    String nama;
+    void membaca(){
+        System.out.println("Mahasiswa membaca");
+    }
+
+    void nyontek(){
+        System.out.println("Mahasiswa menyontek");
+    }
+
+    void modifikasi(){
+        System.out.println("Mahasiswa memoodifikasi");
+    }
+
+    void display (){
+        System.out.println("nama saya : " + nama);
+        System.out.println("NPM saya : " + NPM);
+    }
+}
